25aa7c92f077560d7493dcba947c137ec0871cfe
Gleb Haranin <71976818+GLEF1X@users.noreply.github.com>
2026-08-02 23:22:04 -0400

    Add: time-based-key-value-store - submission-10

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-10.py
@@ -0,0 +1,36 @@
+class TimeMap:
+
+    def __init__(self):
+        self._dict = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        # put it into a dict as [(timestamp, value)]
+        container = self._dict[key]
+        container.append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        container = self._dict.get(key)
+        if not container:
+            return ""
+        left = 0
+        right = len(container) - 1
+        result = ""
+
+        while left <= right:
+            middle = left + (math.ceil((right - left) / 2))
+            probe = container[middle]
+            probeTimestamp  = probe[0]
+
+            if probeTimestamp <= timestamp:
+                result = probe[1]
+                # look for even smaller value
+                left = middle + 1
+            else:
+                # if result:
+                #     return result
+                right = middle - 1
+        
+        return result
+                
+                
+            
