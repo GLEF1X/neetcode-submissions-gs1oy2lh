8e0e32bd77bbf68b39b8a3794291bfe293dd2a78
Gleb Haranin <71976818+GLEF1X@users.noreply.github.com>
2026-08-07 22:46:58 -0400

    Add: longest-repeating-substring-with-replacement - submission-4

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-4.py
@@ -0,0 +1,31 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        left = 0
+        right = 0
+        m = [None] * 26
+        longestSubstringLen = 0
+
+        # XYYX
+
+        while right < len(s):
+            char = s[right]
+            indexToStore = ord(char) - 65
+            if not m[indexToStore]:
+                m[indexToStore] = 0
+            m[indexToStore] += 1
+
+            windowLen = right - left + 1
+            mostFrequentCharOccurencesNum = 0
+            for value in m:
+                mostFrequentCharOccurencesNum = max(value, mostFrequentCharOccurencesNum) if value else mostFrequentCharOccurencesNum
+            
+            numberOfReplacementsRequiredToMaintainValidSubstring = windowLen - mostFrequentCharOccurencesNum
+            if numberOfReplacementsRequiredToMaintainValidSubstring <= k:
+                longestSubstringLen = max(longestSubstringLen, windowLen)
+            else:
+                m[ord(s[left]) - 65] -= 1
+                left += 1
+            
+            right += 1
+            
+        return longestSubstringLen
